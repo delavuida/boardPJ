883877afa4cfb7366f808fe4cb5cf708d175b540
Delavuida <jaju1995@naver.com>
2024-08-19 13:55:28 +0900

    #3 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    기술스택:
    * 스프링 웹
    *엑츄에이터
    *데브툴즈
    *롬복

---
+++ personal-project-board/src/main/java/com/ppb/projectboard/PersonalProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.ppb.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectBoardApplication.class, args);
+	}
+
+}
---
+++ personal-project-board/src/test/java/com/ppb/projectboard/PersonalProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppb.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
